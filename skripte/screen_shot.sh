0b3c38032edf8f444b490f08f7af07510545d6ec
lio.novelli <lio.novelli@agiledrop.com>
2019-09-12 21:48:17 +0200

    Screenshot script added.

---
+++ skripte/screen_shot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# requirements:
+#  - zenity
+#  - import (imagemagick)
+
+scrshot_help() {
+    echo "Open dialog box where you can input file name into which to save the screenshot. If input is left empty /tmp/<currentdate>.png is chosen."
+    #echo -e "\nEXAMPLE"
+}
+
+scrshot_create() {
+    SC_DEFAULT="/tmp/sc-$(date '+%y%m%d-%H%M').png"
+    SC_FILENAME=$(zenity --entry --text "Screenshot name?" --entry-text $SC_DEFAULT --width=400 --height=150)
+    echo "filename: $SC_FILENAME"
+    if [ -z "$SC_FILENAME" ]
+    then
+        # do nothing (wrong input)
+        echo "Canceled."
+    elif [ -f $SC_FILENAME ]
+    then
+        zenity --question --text "Filename $SC_FILENAME already exists. Overwrite it?"
+        echo "overwrite: $SC_OVRWRITE last output: $?"
+        if [ $? -eq 1 ]
+        then
+            #rerun this function
+            scrshot_create
+        else
+            import $SC_FILENAME
+        fi
+    else
+        import $SC_FILENAME
+    fi
+}
+
+scrshot_create
